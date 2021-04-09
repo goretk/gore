2c421de9a6f52edd9331643452356307be13ddad
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2021-04-09 20:19:25 +0100

    Add copyright header

--- type2.go
+++ type2.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The GoRE.tk Authors. All rights reserved.
+// Use of this source code is governed by the license that
+// can be found in the LICENSE file.
+
 package gore
 
 import (
