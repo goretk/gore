f5df9768b8729dbf08ff00dc96c3729d1484b5cb
boyhack <34109680@qq.com>
2021-07-07 15:30:13 +0800

    update test cases

--- goroot_test.go
+++ goroot_test.go
@@ -34,8 +34,36 @@ func TestExtractGoRoot(t *testing.T) {
 			r.NoError(err)
 			defer f.Close()
 			goroot, err := findGoRootPath(f)
-			r.NoError(err)
-			r.Equal(expectGoRoot, goroot)
+			// Windows version 1.5 to 1.9 did not find a goroot string that can be searched, so we ruled it out
+			switch test {
+			case "windows-386-1.5.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-386-1.6.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-386-1.7.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-386-1.7beta1":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-386-1.8.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-386-1.9.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.5.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.6.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.7.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.7beta1":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.8.0":
+				r.Equal(ErrNoGoRootFound, err)
+			case "windows-amd64-1.9.0":
+				r.Equal(ErrNoGoRootFound, err)
+			default:
+				r.NoError(err)
+				r.Equal(expectGoRoot, goroot)
+			}
 		})
 	}
 }
