7ee09d261617ab0fe6d54c8d7b5474d3ba976f4d
Zxilly <zhouxinyu1001@gmail.com>
2024-02-13 18:35:41 +0800

    style: format import and add license header

--- gen/repo.go
+++ gen/repo.go
@@ -1,3 +1,20 @@
+// This file is part of GoRE.
+//
+// Copyright (C) 2019-2024 GoRE Authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -5,8 +22,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/go-git/go-git/v5"
 )
-import "github.com/go-git/go-git/v5"
 
 var goRepo *git.Repository
 
